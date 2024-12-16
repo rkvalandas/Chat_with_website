a2f6e64a8c785b67229db05c2f1cf919c07db4ab
rkvalandas <valandasuramakrishna@gmail.com>
2024-12-16 21:58:44 +0530

    first commit

---
+++ app/chains.py
@@ -0,0 +1,31 @@
+import os
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Chain:
+    def __init__(self):
+        self.llm = ChatGroq(temperature=0, groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.3-70b-versatile")
+
+    def process_query(self, data, query):
+        prompt_extract = PromptTemplate.from_template(
+            """
+            ### SCRAPED TEXT FROM WEBSITE:
+            {page_data}
+            ### INSTRUCTION:
+            You are an assistant tasked with providing relevant information from the above page content based on the following query: {user_query}.
+            ### (NO PREAMBLE):
+            """
+        )
+        query_chain = prompt_extract | self.llm
+        res = query_chain.invoke(input={"page_data": data, "user_query": query})
+        
+        return res.content
+
+
+if __name__ == "__main__":
+    print(os.getenv("GROQ_API_KEY"))
---
+++ app/main.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from langchain_community.document_loaders import WebBaseLoader
+
+from chains import Chain
+from utils import clean_text
+
+
+def create_streamlit_app(llm, clean_text):
+    st.title("🌍 Chat With Any Website")
+    url_input = st.text_input("Enter a URL:", value="https://jobs.nike.com/job/R-33460")
+    query = st.text_input("Enter your query:")
+    submit_button = st.button("Submit")
+
+    if submit_button:
+        try:
+            loader = WebBaseLoader([url_input])
+            data = clean_text(loader.load().pop().page_content)
+            res = llm.process_query(data, query)
+            st.write(res)
+            
+        except Exception as e:
+            st.error(f"An Error Occurred: {e}")
+
+
+if __name__ == "__main__":
+    chain = Chain()
+    st.set_page_config(layout="wide", page_title="Chat with Website", page_icon="📧")
+    create_streamlit_app(chain, clean_text)
+
+
